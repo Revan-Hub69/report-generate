09781aac56e0733ba444cee4c1cfab868500236b
Revan-Hub69 <topofferte@protonmail.ch>
2025-08-01 18:53:39 +0200

    Create tooltipsBlocco4.js

---
+++ src/constants/tooltipsBlocco4.js
@@ -0,0 +1,26 @@
+export const tooltipsBlocco4 = {
+  dividendoAnnuale: {
+    title: "Dividendo Annuale",
+    description: "Importo totale distribuito agli azionisti in un anno per ogni azione."
+  },
+  dividendYield: {
+    title: "Dividend Yield",
+    description: "Rendimento percentuale dato dal dividendo rispetto al prezzo dell’azione."
+  },
+  payoutRatio: {
+    title: "Payout Ratio",
+    description: "Quota degli utili distribuita come dividendo: troppo alto può non essere sostenibile."
+  },
+  buybackYield: {
+    title: "Buyback Yield",
+    description: "Percentuale di azioni riacquistate dall’azienda rispetto alla capitalizzazione."
+  },
+  dividendGrowth5y: {
+    title: "Crescita Dividendo (5 anni)",
+    description: "Media annua di crescita del dividendo negli ultimi 5 anni."
+  },
+  politicaDividendi: {
+    title: "Politica Dividendi",
+    description: "Approccio dichiarato dell’azienda sulla distribuzione di utili (es. stabile, variabile, crescente)."
+  }
+};
