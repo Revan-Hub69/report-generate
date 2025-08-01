8348ea36c974893eeaabc61513f2ddd72f7c6eb1
Revan-Hub69 <topofferte@protonmail.ch>
2025-08-01 18:44:45 +0200

    Create tabLabels.js

---
+++ src/constants/tabLabels.js
@@ -0,0 +1,17 @@
+export const tabLabels = [
+  "Intestazione Report",
+  "Identificativi & Contesto",
+  "Multipli & Valutazione Intrinseca",
+  "Fondamentali Aziendali",
+  "Dividendi & Buyback",
+  "Performance & Rischio",
+  "Microstruttura & Market",
+  "Derivati & Opzioni",
+  "Tecnica & Correlazioni",
+  "Proprietà, ESG & Governance",
+  "Cluster Strategico AI",
+  "Eventi & Macro",
+  "Dati Alternativi & Retail",
+  "Cluster Tradelia AI",
+  "Simulazioni Operative"
+];
